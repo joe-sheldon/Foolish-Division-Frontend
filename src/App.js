21adfb80bac2f7fcd986754697c60667ea96f805
Joseph Sheldon Jr <kb3vwt@gmail.com>
2024-01-22 21:55:42 -0700

    change logo, text

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './public/logo.jpeg';
 import './App.css';
 
 function App() {
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Splitwise got greedy. We're starting this to provide a free, open source alternative.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/kb3vwt/Foolish-Division"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          GitHub Repo
         </a>
       </header>
     </div>
